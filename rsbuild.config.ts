c2427d9fca2bd9038aa826b8aea1e9dd28b180d4
yangxingyuan <yangxingyuan@bytedance.com>
2023-10-30 19:06:23 +0800

    feat: init project

---
+++ rsbuild.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    template: "./src/index.html",
+  },
+  tools: {
+    rspack(config) {
+      config.module?.rules?.unshift({
+        type: "asset/source",
+        resourceQuery: /raw$/,
+      });
+      return config;
+    },
+  },
+});
